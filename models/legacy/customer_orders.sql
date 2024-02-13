80de6ec5da6654b57eb72d0461d0fbd664924079
david_benavides@hakkoda.io <david_benavides@hakkoda.io>
2024-02-13 21:36:05 +0000

    Implementing sources and choosing your refactoring strategy

--- models/legacy/customer_orders.sql
+++ models/legacy/customer_orders.sql
@@ -1,28 +1,26 @@
 WITH paid_orders as (select Orders.ID as order_id,
-    Orders.USER_ID	as customer_id,
+    Orders.USER_ID  as customer_id,
     Orders.ORDER_DATE AS order_placed_at,
         Orders.STATUS AS order_status,
     p.total_amount_paid,
     p.payment_finalized_date,
     C.FIRST_NAME    as customer_first_name,
         C.LAST_NAME as customer_last_name
-FROM raw.jaffle_shop.orders as Orders
+FROM DAVIDBN_DBT_CREDENTIALS.DBT_BENAVIDESDAVID.orders as Orders
 left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from raw.stripe.payment
+        from DAVIDBN_DBT_CREDENTIALS.DBT_BENAVIDESDAVID.payments
         where STATUS <> 'fail'
         group by 1) p ON orders.ID = p.order_id
-left join raw.jaffle_shop.customers C on orders.USER_ID = C.ID ),
-
-customer_orders 
+left join DAVIDBN_DBT_CREDENTIALS.DBT_BENAVIDESDAVID.customers C on orders.USER_ID = C.ID ),
+customer_orders
 as (select C.ID as customer_id
     , min(ORDER_DATE) as first_order_date
     , max(ORDER_DATE) as most_recent_order_date
     , count(ORDERS.ID) AS number_of_orders
-from raw.jaffle_shop.customers C 
-left join raw.jaffle_shop.orders as Orders
-on orders.USER_ID = C.ID 
+from DAVIDBN_DBT_CREDENTIALS.DBT_BENAVIDESDAVID.customers C
+left join DAVIDBN_DBT_CREDENTIALS.DBT_BENAVIDESDAVID.orders as Orders
+on orders.USER_ID = C.ID
 group by 1)
-
 select
 p.*,
 ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
@@ -34,7 +32,7 @@ x.clv_bad as customer_lifetime_value,
 c.first_order_date as fdos
 FROM paid_orders p
 left join customer_orders as c USING (customer_id)
-LEFT OUTER JOIN 
+LEFT OUTER JOIN
 (
         select
         p.order_id,
